0bdf28cabf4c97bb393f49b6fde4a1fbb93e1ee5
chetan7093 <travelp248@gmail.com>
2024-12-29 01:43:59 +0000

    Commit_3.1

--- app/src/main/java/uk/ac/tees/mad/travelplanner/ui/app_navigation/AppNavigation.kt
+++ app/src/main/java/uk/ac/tees/mad/travelplanner/ui/app_navigation/AppNavigation.kt
@@ -1,9 +1,13 @@
 package uk.ac.tees.mad.travelplanner.ui.app_navigation
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import uk.ac.tees.mad.travelplanner.ui.screens.LoginScreen
+import uk.ac.tees.mad.travelplanner.ui.screens.SplashScreen
 
 @Composable
 fun AppNavigation() {
@@ -11,6 +15,9 @@ fun AppNavigation() {
     NavHost(navController = navController, startDestination = Screen.Splash.route) {
         composable(Screen.Splash.route) {
             SplashScreen(navController)
+        }
+        composable(Screen.Login.route) {
+            LoginScreen(navController)
         }
     }
 }
